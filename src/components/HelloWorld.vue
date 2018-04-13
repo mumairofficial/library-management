e9841f2d008f934ce8e032a266e4a1aac9ee5680
Muhammad Umair <mumairofficial@gmail.com>
2018-04-14 00:58:33 +0500

    feat(dashboard widgets in progress):

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,51 +1,65 @@
 <template>
-  <!-- <div class="hello">
-  </div> -->
 
-  <v-layout>
-    <v-flex xs12 sm3 offset-sm3 v-bind="6">
-      <v-card>
-        <v-card-media src="/static/doc-images/cards/desert.jpg" height="200px">
-        </v-card-media>
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-            <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat color="orange">Share</v-btn>
-          <v-btn flat color="orange">Explore</v-btn>
-        </v-card-actions>
-      </v-card>
+  <v-container grid-list-md style="width: 80%;" class="mt-5">
+    <v-layout row wrap>
+      <v-flex xs12 md2 class="mr-3">
+        <v-card hover>
+          <v-card-title>
+            <h3 class="headline">Users</h3>
+            <v-spacer></v-spacer>
+            <v-tooltip top>
+              <v-btn icon class="mr-0" slot="activator">
+                <v-icon>add</v-icon>
+              </v-btn>
+              <span>This is tool tip</span>
+            </v-tooltip>
+            <div class="display-3">150</div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
 
-      <v-card>
-        <v-card-media src="/static/doc-images/cards/desert.jpg" height="200px">
-        </v-card-media>
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-            <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat color="orange">Share</v-btn>
-          <v-btn flat color="orange">Explore</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+      <v-flex xs12 md2 class="mr-3">
+        <v-card hover>
+          <v-card-title>
+            <h3 class="headline">Books</h3>
+            <v-spacer></v-spacer>
+            <v-tooltip top>
+              <v-btn icon class="mr-0" slot="activator">
+                <v-icon>add</v-icon>
+              </v-btn>
+              <span>This is tool tip</span>
+            </v-tooltip>
+            <div class="display-3">100K</div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 md6 class="mr-3">
+        <v-card hover>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>Located two hours south of Sydney in the Southern Highlands of New South Wales,
+                Located two hours south of Sydney in the Southern Highlands of New South Wales, ...</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
