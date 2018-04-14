ff6174bac14f5e3c95809d8358c103e88fe44c57
Muhammad Umair <mumairofficial@gmail.com>
2018-04-14 23:54:18 +0500

    fix(improved left navigation drawer):

--- src/shared/NavigationDrawer.vue
+++ src/shared/NavigationDrawer.vue
@@ -12,61 +12,29 @@
             </v-toolbar-title>
           </v-list-tile-content>
         </v-list-tile>
-
-        <template v-for="item in menuItems">
-          <v-layout
-            row
-            v-if="item.heading"
-            align-center
-            :key="item.heading">
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-            <v-flex xs6 class="text-xs-center">
-              <a href="#!" class="body-2 black--text">EDIT</a>
-            </v-flex>
-          </v-layout>
-
-          <v-list-group
-            v-else-if="item.children"
-            v-model="item.model"
-            :key="item.text"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon="">
-            <v-list-tile slot="activator">
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile v-for="(child, i) in item.children" :key="i"
-              ripple @click="">
-              <v-list-tile-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ child.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list-group>
-
-          <v-list-tile v-else @click="" ripple :key="item.text">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>
-                {{ item.text }}
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
       </v-list>
+
+    <v-list>
+      <v-list-group
+        v-for="item in menuItems"
+        :key="item.title"
+        :prepend-icon="item.action"
+        no-action>
+        <v-list-tile slot="activator">
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile v-for="subItem in item.items" :key="subItem.title" @click="">
+          <v-list-tile-content>
+            <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-icon>{{ subItem.action }}</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list-group>
+    </v-list>
     </v-navigation-drawer>
 </template>
 
@@ -90,10 +58,27 @@ export default {
   data: () => ({
     drawer: null,
     menuItems: [
-      { icon: 'home', text: 'HOME' },
-      { icon: 'supervisor_account', text: 'MANAGE USERS' },
-      { icon: 'library_books', text: 'LIBRARY' },
-      { icon: 'settings', text: 'SETTINGS' }
+      {
+        action: 'home',
+        title: 'HOME'
+      },
+      {
+        action: 'supervisor_account',
+        title: 'MANAGE USERS',
+        active: true,
+        items: [
+          { title: 'Breakfast & brunch' },
+          { title: 'New American' },
+          { title: 'Sushi' }
+        ]
+      },
+      {
+        action: 'settings',
+        title: 'SETTINGS',
+        items: [
+          { title: 'List Item' }
+        ]
+      }
     ]
   })
 }
