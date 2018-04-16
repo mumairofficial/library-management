e3f2b976fa6f2eca876ef8381c7cd2cb784683b6
Muhammad Umair <mumairofficial@gmail.com>
2018-04-16 17:24:51 +0500

    fix(theme aligned to blue accent):

--- src/main.js
+++ src/main.js
@@ -3,20 +3,20 @@
 import Vue from 'vue'
 import router from './router'
 
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-
-// Helpers
-import colors from 'vuetify/es5/util/colors'
-
 import App from './App'
 
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
+
+
 Vue.config.productionTip = false
+
 Vue.use(Vuetify, {
   theme: {
-    primary: colors.teal.accent3, // #E53935
-    secondary: colors.teal.lighten4, // #FFCDD2
-    accent: colors.indigo.base // #3F51B5
+    primary: colors.lightBlue.darken1,
+    secondary: colors.lightBlue.darken3,
+    accent: colors.indigo.base
   }
 });
 
